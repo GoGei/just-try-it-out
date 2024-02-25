37372a0fd7605126c0faa230ab58ecb080d3505b
Richard Bilous <r.bilous@sdh-it.de>
2024-02-25 10:42:16 +0100

    JTIO-Redis-Admin: - Add string commands

--- Admin/Redis/String/urls.py
+++ Admin/Redis/String/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     url(r'set/$', views.redis_string_set, name='admin-redis-string-set'),
     url(r'delete/$', views.redis_string_delete, name='admin-redis-string-delete'),
     url(r'counter/$', views.redis_string_counter, name='admin-redis-string-counter'),
+
+    url(r'get-del/$', views.redis_string_get_del, name='admin-redis-string-get-del'),
+    url(r'get-range/$', views.redis_string_get_range, name='admin-redis-string-get-range'),
+    url(r'get-set/$', views.redis_string_get_set, name='admin-redis-string-get-set'),
+    url(r'lcs/$', views.redis_string_lcs, name='admin-redis-string-lcs'),
 ]
